7d74b566056e6927c7e9330c24c3665fbe8e7b9b
bats <31670615+batscs@users.noreply.github.com>
2024-02-08 22:20:37 +0100

    Update init.sh

--- app/init.sh
+++ app/init.sh
@@ -1,3 +1,3 @@
-echo "* * * * * python3.11 /app/app.py -v $host_name $elastic_domain $elastic_api_key $elastic_index_name > /app/data/app.log" > /app/app.cron
+echo "*/5 * * * * python3.11 /app/app.py -v $host_name $elastic_domain $elastic_api_key $elastic_index_name > /app/data/app.log" > /app/app.cron
 crontab /app/app.cron
 cron -f
